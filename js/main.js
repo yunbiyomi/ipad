1290da92b7f2512dae33431c9c1755955b209437
yunbiyomi <yunbiyomi@naver.com>
2023-07-17 00:52:28 +0900

    💄 Style : 헤더 검색바 구현

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
 
@@ -24,4 +25,46 @@ function showBasket() {
 
 function hideBasket() {
   basketEl.classList.remove('show');
+}
+
+
+// 검색
+const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')];
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+  document.documentElement.classList.add('fixed');
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  });
+  searchDelayEls.forEach((el, index) => {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+  });
+  setTimeout(() => {
+    searchInputEl.focus();
+  }, 600);
+}
+
+function hideSearch() {
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  });
+  searchDelayEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+  });
+  searchDelayEls.reverse();
+  searchInputEl.value = '';
 }
