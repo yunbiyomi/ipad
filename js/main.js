f089f0671b9c10b529ad9edd70b08d4e7a22807c
yunbiyomi <yunbiyomi@naver.com>
2023-07-17 23:56:15 +0900

    💄 Style : Display 섹션 구현

--- js/main.js
+++ js/main.js
@@ -68,3 +68,18 @@ function hideSearch() {
   searchDelayEls.reverse();
   searchInputEl.value = '';
 }
+
+
+// 요소의 가시성 관찰
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return
+    }
+    entry.target.classList.add('show');
+  });
+});
+const infoEls = document.querySelectorAll('.info');
+infoEls.forEach((el) => {
+  io.observe(el);
+});
