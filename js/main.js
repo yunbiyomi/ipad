bc8a192381ec6d769e03c6030714850bfc6a79fc
yunbiyomi <yunbiyomi@naver.com>
2023-07-18 00:37:23 +0900

    ✨ Feat : Video 재생 정지 추가

--- js/main.js
+++ js/main.js
@@ -83,3 +83,21 @@ const infoEls = document.querySelectorAll('.info');
 infoEls.forEach((el) => {
   io.observe(el);
 });
+
+
+// 비디오 재생
+const video = document.querySelector('video');
+const playBtn = document.querySelector('.stage .controller--play');
+const pauseBtn = document.querySelector('.stage .controller--pause');
+
+playBtn.addEventListener('click', () => {
+  video.play();
+  playBtn.classList.add('hide');
+  pauseBtn.classList.remove('hide');
+});
+
+pauseBtn.addEventListener('click', () => {
+  video.pause();
+  playBtn.classList.remove('hide');
+  pauseBtn.classList.add('hide');
+});
