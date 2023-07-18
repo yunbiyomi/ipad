a6f1f0e0d7a4bb5e29aab254287406f69ffa6f64
yunbiyomi <yunbiyomi@naver.com>
2023-07-19 01:05:15 +0900

    💄 Style : 본문 section 마무리

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import ipads from '../data/ipads.js';
+
 // 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
@@ -101,3 +103,32 @@ pauseBtn.addEventListener('click', () => {
   playBtn.classList.remove('hide');
   pauseBtn.classList.add('hide');
 });
+
+
+// '당신에게 맞는 iPad는?' 렌더링
+const itemsEl = document.querySelector('section.compare .items');
+ipads.forEach((ipad) => {
+  const itemEl = document.createElement('div');
+  itemEl.classList.add('item');
+
+  let colorList = '';
+  ipad.colors.forEach((color) => {
+    colorList += `<li style="background-color: ${color};"></li>`
+  })
+
+  itemEl.innerHTML = /* html */ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}">
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩ ${ipad.price.toLocaleString('en-US')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+    `
+
+  itemsEl.append(itemEl);
+})
